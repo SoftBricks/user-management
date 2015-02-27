3ef63bfddd6e6ac4e6c2758dcd96526e82a19e0e
PhilippSpo <philipp.sporrer@planifica.de>
2015-02-27 15:41:23 +0100

    added user subscriptions to group routes

--- server/publications.js
+++ server/publications.js
@@ -1,14 +1,23 @@
-Meteor.publish("users", function () {
-   return publishUsers(this);
+Meteor.publish("users", function() {
+    if (typeof UserManagementTemplates.customPublication === 'function' && arguments[0]) {
+        return UserManagementTemplates.customPublication(arguments);
+    }
+    return publishUsers(this);
 });
 
-publishUsers = function(context){
-    if(context.userId){
-        var user = Meteor.users.findOne({_id: context.userId});
+publishUsers = function(context) {
+    if (context.userId) {
+        var user = Meteor.users.findOne({
+            _id: context.userId
+        });
         if (user.profile.admin === true || user.profile.superAdmin === true) {
             return Meteor.users.find();
         } else {
-            return Meteor.users.find({_id: context.userId});
+            return Meteor.users.find({
+                _id: context.userId
+            });
         }
+    } else {
+        return [];
     }
 };
