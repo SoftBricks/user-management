648fd011659a69330b9ac5115e45d81b5aab4359
philippspo <philipp.sporrer@planifica.de>
2015-10-26 20:25:26 +0100

    add password change callback

--- server/security.js
+++ server/security.js
@@ -1,6 +1,6 @@
 this.Users = Meteor.users;
 //If a user has only the role user he can only change his/her properties except roles
-Security.permit(['update']).collections([Users]).ifHasRole('user').ifIsCurrentUser().exceptProps(['roles']).apply();
+Security.permit(['update']).collections([Users]).ifIsCurrentUser().exceptProps(['roles']).apply();
 //If a user has the role admin and wants to change his own profile he can change all properties except his roles
 Security.permit(['update']).collections([Users]).ifHasRole('admin').ifDoesNotChangeSuperAdmin().ifDoesNotChangeHisRoles().apply();
 //If a user is superAdmin he can change all users except superAdmin users
